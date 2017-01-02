ec9a2c46a2180a7fcd80c0eb2dd92e13f41ca3ef
jtcurr <johntcurrie@gmail.com>
2017-01-02 15:49:33 -0800

    Made initial request for responses

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,16 @@
-// YOUR CODE HERE:
+var request = $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/messages',
+  type: 'GET',
+  //data: JSON.stringify(message),
+  dataType: 'json',
+  contentType: 'application/json',
+  success: function (data) {
+    var message = data.results;
+    console.log('chatterbox: Message received');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
