5a16b3b60b12a50c6039d611709d293fcb397855
jtcurr <johntcurrie@gmail.com>
2017-01-03 12:30:31 -0800

    Troubleshooting adding event handlers to each room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,29 +1,32 @@
 var app = {};
 $('document').ready(function() {
+  var roomArr = [];
   app.server = 'https://api.parse.com/1/classes/messages';
   app.fetch = function() {
+    var room = arguments[0] || "lobby";
     $.ajax({
-  // This is the url you should use to communicate with the parse API server.
       url: app.server,
       type: 'GET',
-         // data: '/calendar/getData.php',
       dataType: 'json',
       data: {
         order: '-createdAt'
       },
       contentType: 'application/json',
       success: function (data) {
-        $('.chats').empty();
+        app.clearMessages();
         var message = data.results;
         console.log('chatterbox: Message received');
-        var roomArr = [];
+        
         for (var i = 0; i < 40; i++) {
           if (roomArr.indexOf(message[i].roomname) === -1) {
             roomArr.push(message[i].roomname);
           }
-          app.renderMessage(message[i]);
+          if(message[i].roomname === room){
+            app.renderMessage(message[i]);
+          }
         }
         app.makeMenu(roomArr);
+        app.clickHandlers(roomArr);
       },
       error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -31,22 +34,37 @@ $('document').ready(function() {
       }  
     });
   };
+
   app.makeMenu = function(arr) {
     $('myDropdown').empty();
   console.log(arr);
     for (var i = 0; i < arr.length; i++) {
-      var item = '<a class ="room" id = "i">' + arr[i] + '</a>'; 
+      var item = '<a class ="room" id =' + i + '>' + arr[i] + '</a>'; 
       $('#myDropdown').append(item);
     }
   };
+
+  app.clickHandlers = function(rooms) {
+    for (var i = 0; i < roomArr.length; i++) {
+      var num = i;
+      var room = "#" + i;
+      $(room).on('click', function(e, num) {
+        console.log(num);
+        app.clearMessages();
+        
+        app.renderRoom(rooms[i]);
+      });
+    }
+  };
+
   app.init = function() {};
 
   app.clearMessages = function() {
     $('.chats').empty();
   };
 
-  app.renderRoom = function() {
-    app.fetch();
+  app.renderRoom = function(room) {
+    app.fetch(room);
   };
   app.renderRoom();
   
